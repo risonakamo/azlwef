7932c3f5a91483a5795ef4970c2a02388a2c1116
risonakamo <hmykanae@gmail.com>
2018-04-19 21:54:00 -0400

    rework popup

--- popup/popup.js
+++ popup/popup.js
@@ -2,11 +2,13 @@ window.onload=main;
 
 function main()
 {
-    var buttons=document.querySelector("a");
-
-    buttons.addEventListener("click",(e)=>{
-        e.preventDefault();
+    chrome.tabs.query({active:true,currentWindow:true},(tab)=>{
+        if (tab[0].url.slice(0,25)!="http://azurlane.wikiru.jp")
+        {
+            return;
+        }
 
         chrome.tabs.executeScript({file:"tablefilter2.js"});
+        document.querySelector(".msg").innerHTML="tables hooked";
     });
 }
